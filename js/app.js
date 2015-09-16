839b7bedf22b0d891829002f73ed929dde2ab554
Maile Lucks <maile.lucks@gmail.com>
2015-09-16 16:52:26 -0700

    Mousedown and Mouseup image set up

--- js/app.js
+++ js/app.js
@@ -2,9 +2,19 @@ $(document).ready(function() {
 	$('.ryu').mouseenter(function(){
 		$('.ryu-still').hide();
 		$('.ryu-ready').show();
-	});
-	$('.ryu').mouseleave(function(){
+	})
+	.mouseleave(function(){
 		$('.ryu-still').show();
 		$('.ryu-ready').hide();
+	})
+	.mousedown(function(){
+		$('.ryu-ready').hide();
+		$('.ryu-throwing').show();
+		$('.hadouken').show();
+	})
+	.mouseup(function(){
+		$('.ryu-ready').show();
+		$('.ryu-throwing').hide();
+		$('.hadouken').hide();
 	});
 });
